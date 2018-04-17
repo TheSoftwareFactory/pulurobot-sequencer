0a2bee1ba827cdbde52f9b828e773481d552b701
Gabriel Gatu <gabriel.dny@gmail.com>
2018-04-17 20:19:50 +0300

    Resolve some bugs

--- src/main.rs
+++ src/main.rs
@@ -24,36 +24,39 @@ impl ReadConnection {
     }
 
     fn start(&self) {
-        self.signal_ch.send(true);
+        self.signal_ch.send(true).unwrap();
     }
 
     fn stop(&self) {
-        self.signal_ch.send(false);
+        self.signal_ch.send(false).unwrap();
     }
 }
 
 fn start_read_thread(signal_ch: Receiver<bool>, data_ch: Sender<u8>) {
     thread::spawn(move || {
         let mut stream: Option<TcpStream> = None;
+        let mut read_mode = false;
         let mut buf: [u8; 1] = [0];
+
         loop {
-            let read_mode = signal_ch.try_recv().unwrap_or(true);
+            read_mode = signal_ch.try_recv().unwrap_or(read_mode);
             match (read_mode, stream.is_some()) {
                 (true, false) => {
                     stream = Some(TcpStream::connect(ROBOT_IP).unwrap());
-                    stream.as_mut().unwrap().read(&mut buf);
-                    data_ch.send(buf[0]);
+                    stream.as_mut().unwrap().read(&mut buf).unwrap();
+                    data_ch.send(buf[0]).unwrap();
                 }
                 (true, true) => {
-                    stream.as_mut().unwrap().read(&mut buf);
-                    data_ch.send(buf[0]);
+                    stream.as_mut().unwrap().read(&mut buf).unwrap();
+                    data_ch.send(buf[0]).unwrap();
                 }
                 (false, false) => (),
                 (false, true) => {
                     stream
                         .as_mut()
                         .unwrap()
-                        .shutdown(::std::net::Shutdown::Both);
+                        .shutdown(::std::net::Shutdown::Both)
+                        .unwrap();
                     stream = None;
                 }
             }
@@ -98,27 +101,30 @@ fn start_write_thread(
 ) {
     thread::spawn(move || {
         let mut stream: Option<TcpStream> = None;
+        let mut write_mode = false;
+
         loop {
-            let write_mode = signal_ch.try_recv().unwrap_or(false);
+            write_mode = signal_ch.try_recv().unwrap_or(write_mode);
             match (write_mode, stream.is_some()) {
                 (true, false) => {
                     stream = Some(TcpStream::connect(ROBOT_IP).unwrap());
 
                     let data: Vec<u8> = data_ch.recv().unwrap();
-                    stream.as_mut().unwrap().write_all(&data[..]);
-                    completed_ch.send(true);
+                    stream.as_mut().unwrap().write_all(&data[..]).unwrap();
+                    completed_ch.send(true).unwrap();
                 }
                 (true, true) => {
                     let data: Vec<u8> = data_ch.recv().unwrap();
-                    stream.as_mut().unwrap().write_all(&data[..]);
-                    completed_ch.send(true);
+                    stream.as_mut().unwrap().write_all(&data[..]).unwrap();
+                    completed_ch.send(true).unwrap();
                 }
                 (false, false) => (),
                 (false, true) => {
                     stream
                         .as_mut()
                         .unwrap()
-                        .shutdown(::std::net::Shutdown::Both);
+                        .shutdown(::std::net::Shutdown::Both)
+                        .unwrap();
                     stream = None;
                 }
             };
@@ -135,34 +141,46 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.set_read_timeout(Some(::std::time::Duration::from_millis(10)));
                 println!("Got connection");
+                stream.set_read_timeout(Some(::std::time::Duration::from_millis(10))).unwrap();
+                stream.set_nodelay(true).unwrap();
 
                 loop {
+                    stream.set_nonblocking(true).unwrap();
                     let mut buf: Vec<u8> = Vec::new();
+
                     match stream.read_to_end(&mut buf).is_ok() && buf.len() > 0 {
                         true => {
                             read_conn.stop();
                             write_conn.start();
 
                             println!("buf {:?}", buf);
-                            write_conn.data_ch.send(buf);
+                            write_conn.data_ch.send(buf).unwrap();
+
+                            let completed = write_conn.completed_ch.recv().expect("write conn");
+                            println!("completed write: {:?}", completed);
+
+                            if completed {
+                                read_conn.start();
+                                write_conn.stop();
+                            }
                         }
                         false => {
                             read_conn.start();
                             write_conn.stop();
 
-                            let data = read_conn.data_ch.recv().unwrap();
+                            let data = read_conn.data_ch.recv().expect("data receive failed");
 
                             println!("data {:?}", data);
 
                             let slice = vec![data];
                             println!("slice {:?}", slice);
 
-                            stream.write(&slice[..]).unwrap();
-                            stream.flush();
+                            stream.set_nonblocking(false).unwrap();
+                            stream.write(&slice[..]).expect("write slice failed");
+                            stream.flush().expect("flush slice failed");
                         }
-                    }
+                    };
                 }
             }
             Err(e) => {
@@ -170,38 +188,4 @@ fn main() {
             }
         }
     }
-
-    // let mut stream = TcpStream::connect("192.168.43.97:22222").unwrap();
-    // let x = 1000;
-    // let y = 1000;
-
-    // let mut buf: [u8; 11] = [0; 11];
-    // buf[0] = 56;
-    // buf[1] = 0;
-    // buf[2] = 9;
-
-    // buf[3] = (x << 24) as u8;
-    // buf[4] = (x << 16) as u8;
-    // buf[5] = (x << 8) as u8;
-    // buf[6] = (x << 0) as u8;
-
-    // buf[7] = (y << 24) as u8;
-    // buf[8] = (y << 16) as u8;
-    // buf[9] = (y << 8) as u8;
-    // buf[10] = (y << 0) as u8;
-
-    // stream.write(&buf);
-
-    // let listener = TcpListener::bind("192.168.43.97:22222").unwrap();
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(mut stream) => {
-    //             let mut buf: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    //             stream.write(&buf[..]);
-    //         }
-    //         Err(e) => {
-    //             println!("connection failed");
-    //          }
-    //     }
-    // }
 }
